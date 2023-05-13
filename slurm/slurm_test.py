5844b879b53f3b9c21cd5d6d44e6cab70d93b48a
Julian Grohmann <juliangrohmann@protonmail.com>
2023-05-13 15:33:41 -0400

    update slurm_test

--- slurm/slurm_test.py
+++ slurm/slurm_test.py
@@ -1,8 +1,11 @@
 import numpy as np
 import sys
-
-from parametric_solver.solver import BilinearSolver
+import pkgutil
 
 sys.path.append('../parametric_solver')
+
+# Iterate over all modules in the Python path
+for module in pkgutil.iter_modules():
+    print(module.name)
 
 print("TESTING")
