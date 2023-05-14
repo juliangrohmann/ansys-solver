89715913ff93fe2cd8c137d77e0beff432608b56
Julian Grohmann <juliangrohmann@protonmail.com>
2023-05-13 21:49:47 -0400

    update slurm_test

--- slurm/slurm_test.py
+++ slurm/slurm_test.py
@@ -4,11 +4,14 @@ import os
 import pkgutil
 
 HOME = os.path.expanduser('~')
-INP_PATH = sys.path.append(os.path.join(HOME, 'scratch', 'inp', 'hemj_v2.inp'))
+INP_PATH = os.path.join(HOME, 'scratch', 'inp')
+HEMJ_INP = os.path.join(INP_PATH, "hemj_v2.inp")
+
+sys.path.append(INP_PATH)
 
 from parametric_solver.solver import BilinearSolver
 
 
-solver = BilinearSolver(INP_PATH)
+solver = BilinearSolver(HEMJ_INP)
 solver.add_sample(200e9, 700e6, 70e9)
 solver.solve()
