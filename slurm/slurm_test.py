f89031d724b7f91da9fbf2bfed9b959337e6f56f
Julian Grohmann <juliangrohmann@protonmail.com>
2023-05-13 22:13:35 -0400

    update slurm_test

--- slurm/slurm_test.py
+++ slurm/slurm_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 import os
 import pkgutil
+from ansys.mapdl import core as pymapdl
 
 HOME = os.path.expanduser('~')
 SCRATCH_PATH = os.path.join(HOME, 'scratch')
@@ -14,19 +15,9 @@ sys.path.append(SCRATCH_PATH)
 from parametric_solver.solver import BilinearSolver
 
 
-def print_directory_tree(start_path):
-    for root, dirs, files in os.walk(start_path):
-        level = root.replace(start_path, '').count(os.sep)
-        indent = ' ' * 4 * level
-        print(f'{indent}{os.path.basename(root)}/')
-        sub_indent = ' ' * 4 * (level + 1)
-        for f in files:
-            print(f'{sub_indent}{f}')
+print(f"Ansys executable = {ANSYS_EXE}")
+pymapdl.change_default_ansys_path(ANSYS_EXE)
 
-
-print(f"ANSYS ROOT = {ANSYS_ROOT}")
-print_directory_tree(ANSYS_ROOT)
-
-# solver = BilinearSolver(HEMJ_INP)
-# solver.add_sample(200e9, 700e6, 70e9)
-# solver.solve()
+solver = BilinearSolver(HEMJ_INP)
+solver.add_sample(200e9, 700e6, 70e9)
+solver.solve()
