17c00b176706d2bc38557be2714bf76e3b0f1c8d
Julian Grohmann <juliangrohmann@protonmail.com>
2023-05-15 01:38:30 -0400

    update sbatch

--- slurm/launch_client.sh
+++ slurm/launch_client.sh
@@ -9,13 +9,7 @@ rm -rf *
 #echo "Launched gRPC server on node ${SLURM_NODEID}, process ${SLURM_PROCID}"
 
 module load ansys/2023R1
-module unload intel/20.0.4
-module load openmpi/4.1.4
-#module load mvapich2/2.3.6-ouywal
-
-export OMPI_MCA_btl_base_warn_component_unused=0
-export OMPI_MCA_mpi_warn_on_fork=0
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/pace-apps/manual/packages/openmpi/4.1.4/gcc-10.3.0/lib
+module load mvapich2/2.3.6-ouywal
 
 "/usr/local/pace-apps/manual/packages/ansys/2023R1/v231/ansys/bin/ansys231" -j file -port 50052 -grpc
 
