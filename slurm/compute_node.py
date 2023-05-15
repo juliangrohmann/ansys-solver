d9f7ef8374c0e7b8062cabaf4d3b883fc0bc38be
Julian Grohmann <juliangrohmann@protonmail.com>
2023-05-14 20:15:53 -0400

    use multiple nodes

--- slurm/compute_node.py
+++ slurm/compute_node.py
@@ -2,7 +2,6 @@ import os
 import env
 
 SCRATCH_PATH = env.init_scratch()
-# SCRATCH_PATH = env.init_root()
 from parametric_solver.solver import BilinearSolver
 from parametric_solver.client import SolverClient
 
@@ -11,11 +10,9 @@ HEMJ_INP = os.path.join(SCRATCH_PATH, 'inp', 'hemj_v2.inp')
 OUTPUR_DIR = os.path.join(SCRATCH_PATH, 'output')
 SOLUTION_DIR = os.path.join(OUTPUR_DIR, 'solutions')
 
-RUN_DIR = os.path.join(OUTPUR_DIR, os.environ.get("SLURM_JOB_ID"))
-# RUN_DIR = os.path.join(OUTPUR_DIR, 'err', '1')
+RUN_DIR = os.path.join(OUTPUR_DIR, 'err', os.environ.get("SLURM_JOB_ID"))
 
 SERVER_IP = '128.61.254.34:41559'
-# SERVER_IP = '127.0.0.1:5000'
 
 env.init_ansys()
 
