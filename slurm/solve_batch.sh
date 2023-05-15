d03d6904c271629502bd6c91709946952547df63
Julian Grohmann <juliangrohmann@protonmail.com>
2023-05-15 02:27:58 -0400

    add -n flag

--- slurm/solve_batch.sh
+++ slurm/solve_batch.sh
@@ -1,3 +1,19 @@
+while getopts ":n:" opt; do
+  case $opt in
+    n)
+      num_iterations="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
 mkdir -p ~/scratch/output/err
 mkdir -p ~/scratch/output/solutions
 cd  ~/scratch/output/err
@@ -5,8 +21,8 @@ rm -rf *
 
 cd ~/scratch/slurm
 
-for i in {1..4}; do
-    sbatch launch_client.sbatch
+for ((i=1; i<=num_iterations; i++)); do
+  sbatch launch_client.sbatch
 done
 
 python head_node.py
