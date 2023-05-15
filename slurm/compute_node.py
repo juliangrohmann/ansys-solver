be5f0f0875d9dd5579a1a8784a6afd499aca11a2
Julian Grohmann <juliangrohmann@protonmail.com>
2023-05-14 20:27:25 -0400

    update slurm test

--- slurm/compute_node.py
+++ slurm/compute_node.py
@@ -9,9 +9,7 @@ from parametric_solver.client import SolverClient
 HEMJ_INP = os.path.join(SCRATCH_PATH, 'inp', 'hemj_v2.inp')
 OUTPUR_DIR = os.path.join(SCRATCH_PATH, 'output')
 SOLUTION_DIR = os.path.join(OUTPUR_DIR, 'solutions')
-
 RUN_DIR = os.path.join(OUTPUR_DIR, 'err', os.environ.get("SLURM_JOB_ID"))
-
 SERVER_IP = '128.61.254.34:41559'
 
 env.init_ansys()
