8db49bf0a68ddc91daacdde6f8dba192624df129
Julian Grohmann <juliangrohmann@protonmail.com>
2023-08-25 13:16:35 -0400

    sh

--- analysis_v3/slurm/solve_batch.sh
+++ analysis_v3/slurm/solve_batch.sh
@@ -7,30 +7,19 @@ mkdir out_flat_plastic
 
 cd ~/scratch/ansys_solver/analysis_v3/slurm
 
-sbatch launch_client.sbatch curved elastic 0 50
-sbatch launch_client.sbatch curved elastic 50 100
-
-sbatch launch_client.sbatch flat elastic 0 50
-sbatch launch_client.sbatch flat elastic 50 100
-
-sbatch launch_client.sbatch curved plastic 0 50
-sbatch launch_client.sbatch curved plastic 50 100
-sbatch launch_client.sbatch curved plastic 100 150
-sbatch launch_client.sbatch curved plastic 150 200
-sbatch launch_client.sbatch curved plastic 200 250
-sbatch launch_client.sbatch curved plastic 250 300
-sbatch launch_client.sbatch curved plastic 300 350
-sbatch launch_client.sbatch curved plastic 350 400
-sbatch launch_client.sbatch curved plastic 400 450
-sbatch launch_client.sbatch curved plastic 450 500
-
-sbatch launch_client.sbatch flat plastic 0 50
-sbatch launch_client.sbatch flat plastic 50 100
-sbatch launch_client.sbatch flat plastic 100 150
-sbatch launch_client.sbatch flat plastic 150 200
-sbatch launch_client.sbatch flat plastic 200 250
-sbatch launch_client.sbatch flat plastic 250 300
-sbatch launch_client.sbatch flat plastic 300 350
-sbatch launch_client.sbatch flat plastic 350 400
-sbatch launch_client.sbatch flat plastic 400 450
-sbatch launch_client.sbatch flat plastic 450 500
+sbatch launch_client.sbatch curved plastic 0 25
+sbatch launch_client.sbatch curved plastic 25 50
+sbatch launch_client.sbatch curved plastic 50 75
+sbatch launch_client.sbatch curved plastic 75 100
+sbatch launch_client.sbatch curved plastic 100 125
+sbatch launch_client.sbatch curved plastic 125 150
+sbatch launch_client.sbatch curved plastic 150 175
+sbatch launch_client.sbatch curved plastic 175 200
+sbatch launch_client.sbatch curved plastic 200 225
+sbatch launch_client.sbatch curved plastic 225 250
+sbatch launch_client.sbatch curved plastic 250 275
+sbatch launch_client.sbatch curved plastic 275 300
+sbatch launch_client.sbatch curved plastic 300 325
+sbatch launch_client.sbatch curved plastic 325 350
+sbatch launch_client.sbatch curved plastic 350 375
+sbatch launch_client.sbatch curved plastic 375 391
