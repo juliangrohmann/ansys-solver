84a8faf38c49855b0c8fff1b3249c0d6f384db43
Julian Grohmann <juliangrohmann@protonmail.com>
2023-08-26 02:29:03 -0400

    switch flat

--- analysis_v3/slurm/solve_batch.sh
+++ analysis_v3/slurm/solve_batch.sh
@@ -7,19 +7,19 @@ mkdir out_flat_plastic
 
 cd ~/scratch/ansys_solver/analysis_v3/slurm
 
-sbatch launch_client.sbatch curved elastic 0 25
-sbatch launch_client.sbatch curved elastic 25 50
-sbatch launch_client.sbatch curved elastic 50 75
-sbatch launch_client.sbatch curved elastic 75 100
-sbatch launch_client.sbatch curved elastic 100 125
-sbatch launch_client.sbatch curved elastic 125 150
-sbatch launch_client.sbatch curved elastic 150 175
-sbatch launch_client.sbatch curved elastic 175 200
-sbatch launch_client.sbatch curved elastic 200 225
-sbatch launch_client.sbatch curved elastic 225 250
-sbatch launch_client.sbatch curved elastic 250 275
-sbatch launch_client.sbatch curved elastic 275 300
-sbatch launch_client.sbatch curved elastic 300 325
-sbatch launch_client.sbatch curved elastic 325 350
-sbatch launch_client.sbatch curved elastic 350 375
-sbatch launch_client.sbatch curved elastic 375 391
+sbatch launch_client.sbatch flat plastic 0 25
+sbatch launch_client.sbatch flat plastic 25 50
+sbatch launch_client.sbatch flat plastic 50 75
+sbatch launch_client.sbatch flat plastic 75 100
+sbatch launch_client.sbatch flat plastic 100 125
+sbatch launch_client.sbatch flat plastic 125 150
+sbatch launch_client.sbatch flat plastic 150 175
+sbatch launch_client.sbatch flat plastic 175 200
+sbatch launch_client.sbatch flat plastic 200 225
+sbatch launch_client.sbatch flat plastic 225 250
+sbatch launch_client.sbatch flat plastic 250 275
+sbatch launch_client.sbatch flat plastic 275 300
+sbatch launch_client.sbatch flat plastic 300 325
+sbatch launch_client.sbatch flat plastic 325 350
+sbatch launch_client.sbatch flat plastic 350 375
+sbatch launch_client.sbatch flat plastic 375 391
